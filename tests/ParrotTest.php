a6598748387dcd07bd013de12c139c285dea5177
Zacarías <zacarias@wichipu.com>
2024-02-03 17:12:22 +0100

    test: Added argument names.

--- tests/ParrotTest.php
+++ tests/ParrotTest.php
@@ -23,73 +23,133 @@ class ParrotTest extends TestCase
 
     public function testSpeedOfAfricanParrotWithOneCoconut(): void
     {
-        $parrot = $this->getParrot(ParrotTypeEnum::AFRICAN, 1, 0, false);
+        $parrot = $this->getParrot(
+            type: ParrotTypeEnum::AFRICAN,
+            numberOfCoconuts: 1,
+            voltage: 0,
+            isNailed: false
+        );
         self::assertSame(3.0, $parrot->getSpeed());
     }
 
     public function testSpeedOfAfricanParrotWithTwoCoconuts(): void
     {
-        $parrot = $this->getParrot(ParrotTypeEnum::AFRICAN, 2, 0, false);
+        $parrot = $this->getParrot(
+            type: ParrotTypeEnum::AFRICAN,
+            numberOfCoconuts: 2,
+            voltage: 0,
+            isNailed: false
+        );
         self::assertSame(0.0, $parrot->getSpeed());
     }
 
     public function testSpeedOfAfricanParrotWithNoCoconuts(): void
     {
-        $parrot = $this->getParrot(ParrotTypeEnum::AFRICAN, 0, 0, false);
+        $parrot = $this->getParrot(
+            type: ParrotTypeEnum::AFRICAN,
+            numberOfCoconuts: 0,
+            voltage: 0,
+            isNailed: false
+        );
         self::assertSame(12.0, $parrot->getSpeed());
     }
 
     public function testSpeedNorwegianBlueParrotNailed(): void
     {
-        $parrot = $this->getParrot(ParrotTypeEnum::NORWEGIAN_BLUE, 0, 1.5, true);
+        $parrot = $this->getParrot(
+            type: ParrotTypeEnum::NORWEGIAN_BLUE,
+            numberOfCoconuts: 0,
+            voltage: 1.5,
+            isNailed: true
+        );
         self::assertSame(0.0, $parrot->getSpeed());
     }
 
     public function testSpeedNorwegianBlueParrotNotNailed(): void
     {
-        $parrot = $this->getParrot(ParrotTypeEnum::NORWEGIAN_BLUE, 0, 1.5, false);
+        $parrot = $this->getParrot(
+            type: ParrotTypeEnum::NORWEGIAN_BLUE,
+            numberOfCoconuts: 0,
+            voltage: 1.5,
+            isNailed: false
+        );
         self::assertSame(18.0, $parrot->getSpeed());
     }
 
     public function testSpeedNorwegianBlueParrotNotNailedHighVoltage(): void
     {
-        $parrot = $this->getParrot(ParrotTypeEnum::NORWEGIAN_BLUE, 0, 4, false);
+        $parrot = $this->getParrot(
+            type: ParrotTypeEnum::NORWEGIAN_BLUE,
+            numberOfCoconuts: 0,
+            voltage: 4,
+            isNailed: false
+        );
         self::assertSame(24.0, $parrot->getSpeed());
     }
 
     public function testAnUnknownParrotWillWillThrownAnException(): void
     {
         $this->expectExceptionMessage('Should be unreachable');
-        $unknownParrot = new Parrot(-1, 0, 0, false);
+        $unknownParrot = new Parrot(
+            type: -1,
+            numberOfCoconuts: 0,
+            voltage: 0,
+            isNailed: false
+        );
         $unknownParrot->getSpeed();
     }
 
     public function testGetCryOfEuropeanParrot(): void
     {
-        $parrot = $this->getParrot(ParrotTypeEnum::EUROPEAN, 0, 0, false);
+        $parrot = $this->getParrot(
+            type: ParrotTypeEnum::EUROPEAN,
+            numberOfCoconuts: 0,
+            voltage: 0,
+            isNailed: false
+        );
         self::assertSame('Sqoork!', $parrot->getCry());
     }
 
     public function testGetCryOfAfricanParrot(): void
     {
-        $parrot = $this->getParrot(ParrotTypeEnum::AFRICAN, 1, 0, false);
+        $parrot = $this->getParrot(
+            type: ParrotTypeEnum::AFRICAN,
+            numberOfCoconuts: 1,
+            voltage: 0,
+            isNailed: false
+        );
         self::assertSame('Sqaark!', $parrot->getCry());
     }
 
     public function testGetCryOfNorwegianBlueHighVoltage(): void
     {
-        $parrot = $this->getParrot(ParrotTypeEnum::NORWEGIAN_BLUE, 0, 4, false);
+        $parrot = $this->getParrot(
+            type: ParrotTypeEnum::NORWEGIAN_BLUE,
+            numberOfCoconuts: 0,
+            voltage: 4,
+            isNailed: false
+        );
         self::assertSame('Bzzzzzz', $parrot->getCry());
     }
 
     public function testGetCryOfNorwegianBlueNoVoltage(): void
     {
-        $parrot = $this->getParrot(ParrotTypeEnum::NORWEGIAN_BLUE, 0, 0, false);
+        $parrot = $this->getParrot(
+            type: ParrotTypeEnum::NORWEGIAN_BLUE,
+            numberOfCoconuts: 0,
+            voltage: 0,
+            isNailed: false
+        );
         self::assertSame('...', $parrot->getCry());
     }
 
     private function getParrot(int $type, int $numberOfCoconuts, float $voltage, bool $isNailed): Parrot
     {
-        return new Parrot($type, $numberOfCoconuts, $voltage, $isNailed);
+        return new Parrot(
+            type: $type,
+            numberOfCoconuts: $numberOfCoconuts,
+            voltage: $voltage,
+            isNailed: $isNailed
+        );
     }
 }
